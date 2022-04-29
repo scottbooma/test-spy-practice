4f5897301427c1219bc486ded8952b57b5a8aa26
scottbooma <scottbooma@gmail.com>
2022-04-29 12:23:43 -0400

    Add passing test for filter

--- index.test.js
+++ index.test.js
@@ -11,3 +11,16 @@ test("map returns a transformed array of items when given an array of multiple i
 
     expect(transformedArray).toEqual([2, 4, 6])
 })
+
+test("filter returns an array of matching values when given an array of multiple items", () => {
+    const array = [1, 2, 2, 3]
+    const predicate = jest.fn()
+        .mockReturnValueOnce(false)
+        .mockReturnValueOnce(true)
+        .mockReturnValueOnce(true)
+        .mockReturnValueOnce(false)
+
+    const filteredArray = filter(array, predicate)
+
+    expect(filteredArray).toEqual([2, 2])
+})
